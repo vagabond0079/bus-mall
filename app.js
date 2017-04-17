aed329288a6734f77db931138b501a7e0ce93bd8
derekcfarmer <derek.farmer@gmail.com>
2017-04-17 15:53:39 -0700

    planned out app.js from user stories and project specs

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello world');
+/* ========== Product-img Object Contstructor ========== */
+
+/* ========== Random Product-img Selector ========== */
+
+/* ========== Render Random Product-img ========== */
+
+/* ========== Click Receiver ========== */
+
+/* ========== Click Tracker ========== */
+
+/* ========== Product-img Display Tracker ========== */
+
+/* ========== Event Listener ========== */
+
+/* ========== Render List of Products with Votes Received ========== */
