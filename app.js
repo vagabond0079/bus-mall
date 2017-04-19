fc53533d72720dffbc2bd8ee91004a3f50ea41a3
derekcfarmer <derek.farmer@gmail.com>
2017-04-18 22:29:26 -0700

    chart looks good. noticed the three cycle I built doesnt prevent dupes 100 percent of the time.

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 
 /* ========== Global variables ========== */
 
-var products = [];
 var currentProductsDisplayed = [];
-var randomNumber;
-var prevRandomNumber;
-var firstRandomNumber;
 var totalClicks = 0;
 var threeCycle = 1;
+var randomNumber, prevRandomNumber, firstRandomNumber;
 
 /* ========== Product-img Object Contstructor ========== */
 
@@ -21,31 +18,29 @@ function Product(name, imgName, htmlId){
   this.previouslyDisplayed = false;
 }
 
-
 /* ========== Create Product-img Objects ========== */
 
-products[0] = new Product('R2D2 Rolling Suitcase', 'bag.jpg', 'bag');
-products[1] = new Product('Banana Slicer', 'banana.jpg', 'banana');
-products[2] = new Product('Bathroom iPad Holder', 'bathroom.jpg', 'bathroom');
-products[3] = new Product('Toe-less Boots', 'boots.jpg', 'boots');
-products[4] = new Product('All-in-One Breakfast Station', 'breakfast.jpg', 'breakfast');
-products[5] = new Product('Italian Meatball Bubblegum', 'bubblegum.jpg', 'bubblegum');
-products[6] = new Product('Humpback Chair', 'chair.jpg', 'chair');
-products[7] = new Product('Cthulu Figure', 'cthulhu.jpg', 'cthulu');
-products[8] = new Product('Duck Mask for Dogs', 'dog-duck.jpg', 'dog-duck');
-products[9] = new Product('Dragon Meat', 'dragon.jpg', 'dragon');
-products[10] = new Product('Pen Cap Flatware', 'pen.jpg', 'pen');
-products[11] = new Product('Pet Sweep', 'pet-sweep.jpg', 'pet-sweep');
-products[12] = new Product('Pizza Scissors', 'scissors.jpg', 'scissors');
-products[13] = new Product('Shark Sleeping Bag', 'shark.jpg', 'shark');
-products[14] = new Product('Baby Sweepwer Onesie', 'sweep.png', 'sweep');
-products[15] = new Product('Tauntaun Sleeping Bag', 'tauntaun.jpg', 'tauntaun');
-products[16] = new Product('Unicorn Meat', 'unicorn.jpg', 'unicorn');
-products[17] = new Product('Tentacle USB Flash Drive', 'usb.gif', 'usb');
-products[18] = new Product('Reversed Watering Can', 'water-can.jpg', 'water-can');
-products[19] = new Product('Novelty Wine Glass', 'wine-glass.jpg', 'wine-glass');
-
-
+var products = [
+  new Product('R2D2 Rolling Suitcase', 'bag.jpg', 'bag'),
+  new Product('Banana Slicer', 'banana.jpg', 'banana'),
+  new Product('Bathroom iPad Holder', 'bathroom.jpg', 'bathroom'),
+  new Product('Toe-less Boots', 'boots.jpg', 'boots'),
+  new Product('All-in-One Breakfast Station', 'breakfast.jpg', 'breakfast'),
+  new Product('Italian Meatball Bubblegum', 'bubblegum.jpg', 'bubblegum'),
+  new Product('Humpback Chair', 'chair.jpg', 'chair'),
+  new Product('Cthulu Figure', 'cthulhu.jpg', 'cthulu'),
+  new Product('Duck Mask for Dogs', 'dog-duck.jpg', 'dog-duck'),
+  new Product('Dragon Meat', 'dragon.jpg', 'dragon'),
+  new Product('Pen Cap Flatware', 'pen.jpg', 'pen'),
+  new Product('Pet Sweep', 'pet-sweep.jpg', 'pet-sweep'),
+  new Product('Pizza Scissors', 'scissors.jpg', 'scissors'),
+  new Product('Shark Sleeping Bag', 'shark.jpg', 'shark'),
+  new Product('Baby Sweepwer Onesie', 'sweep.png', 'sweep'),
+  new Product('Tauntaun Sleeping Bag', 'tauntaun.jpg', 'tauntaun'),
+  new Product('Unicorn Meat', 'unicorn.jpg', 'unicorn'),
+  new Product('Tentacle USB Flash Drive', 'usb.gif', 'usb'),
+  new Product('Reversed Watering Can', 'water-can.jpg', 'water-can'),
+  new Product('Novelty Wine Glass', 'wine-glass.jpg', 'wine-glass'),];
 
 /* ========== Random Product-img Chooser ========== */
 
@@ -53,10 +48,10 @@ function randomProductChooser(){
 
   for(var i = 0; i < 3; i++){
     randomNumber = Math.floor (Math.random() * (products.length));
-    console.log('randomNumber is ' + randomNumber, 'prevRandomNumber is ' + prevRandomNumber, 'firstRandomNumber is ' + firstRandomNumber);
     if(randomNumber != prevRandomNumber && randomNumber != firstRandomNumber && !products[randomNumber].previouslyDisplayed){
       currentProductsDisplayed[i] = products[randomNumber];
       products[randomNumber].numberTimesShown++;
+
       firstRandomNumber = prevRandomNumber;
       prevRandomNumber = randomNumber;
       products[randomNumber].previouslyDisplayed = true;
@@ -88,7 +83,6 @@ function handleImg0Click(e) {
   currentProductsDisplayed[0].numberTimesClicked++;
 
   totalClicks++;
-  console.log('totalClicks is ' + totalClicks);
 
   if(totalClicks === 25){
     img0.removeEventListener('click', handleImg0Click);
@@ -123,7 +117,6 @@ function handleImg1Click(e) {
   currentProductsDisplayed[1].numberTimesClicked++;
 
   totalClicks++;
-  console.log('totalClicks is ' + totalClicks);
 
   if(totalClicks === 25){
     img0.removeEventListener('click', handleImg0Click);
@@ -158,7 +151,6 @@ function handleImg2Click(e) {
   currentProductsDisplayed[2].numberTimesClicked++;
 
   totalClicks++;
-  console.log('totalClicks is ' + totalClicks);
 
   if(totalClicks === 25){
     img0.removeEventListener('click', handleImg0Click);
@@ -197,34 +189,10 @@ var img2 = document.getElementById('img2');
 img2.addEventListener('click', handleImg2Click);
 
 
-/* ========== Render List of Products with Votes Received ========== */
-//function displayResults() {
-//  console.log('nothing to see here');
-
-//   for(var i = 0; i < products.length; i++){
-//     var newEl = document.createElement('h2');
-//     newEl.id = products[i] + 'header';
-//     var newText = document.createTextNode(products[i].name);
-//     newEl.appendChild(newText);
-//     var position = document.getElementById('tableWrap');
-//     position.appendChild(newEl);
-//
-//     newEl = document.createElement('p');
-//     newEl.id = products[i] + 'text';
-//     newText = document.createTextNode('Shown ' + products[i].numberTimesShown + ' times. ');
-//     newEl.appendChild(newText);
-//     newText = document.createTextNode('Clicked ' + products[i].numberTimesClicked + ' times. ');
-//     newEl.appendChild(newText);
-//     var percentageClicked = Math.round((products[i].numberTimesClicked / products[i].numberTimesShown) * 100);
-//     newText = document.createTextNode('Clicked ' + percentageClicked + ' percent of the time');
-//     newEl.appendChild(newText);
-//     position = document.getElementById('tableWrap');
-//     position.appendChild(newEl);
-//   }
-//}
-// ========== Bar Chart of Results ==========
+// ========== Bar Chart of Results: Votes Received ==========
 
 function displayResults() {
+  //create canvas element
   var newEl = document.createElement('canvas');
   newEl.id = 'resultsChart';
   var position = document.getElementById('app');
@@ -232,23 +200,48 @@ function displayResults() {
 
   var ctx = document.getElementById('resultsChart').getContext('2d');
 
-  // var productNames = [];
+  //create array of productNames from product[i].names for use in chart labels.
+  var productNames = [];
+
+  function productsToProductNames() {
+    for(var i = 0; i < products.length; i++){
+      productNames.push(products[i].name);
+    }
+  }
+
+  productsToProductNames();
+
+  //create array of productVotes from product[i].numberTimesClicked for use in chart data.
+  var productVotes = [];
+
+  function productsToProductVotes() {
+    for(var i = 0; i < products.length; i++){
+      productVotes.push(products[i].numberTimesClicked);
+    }
+  }
+
+  productsToProductVotes();
+
+  //HOLD: FIGUREING OUT HOW TO HAVE LAYERED BAR GRAPHS.
+  //create array of productShows from product[i].numberTimesShown for use in chart data.
+  // var productShows = [];
   //
-  // function productsToProductNames() {
+  // function productsToProductShows() {
   //   for(var i = 0; i < products.length; i++){
-  //     productNames.push(products[i].name);
+  //     productShows.push(products[i].numberTimesShown);
   //   }
   // }
-  //
-  // productsToProductNames();
 
+  // productsToProductShows();
+
+  //create chart.
   var resultsChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-      labels: [products[0].name, products[1].name, products[2].name, products[3].name, products[4].name, products[5].name, products[6].name, products[7].name, products[8].name, products[9].name, products[10].name, products[11].name, products[12].name, products[13].name, products[14].name, products[15].name, products[16].name, products[17].name, products[18].name, products[19].name],
+      labels: productNames,
       datasets: [{
         label: '# of Votes',
-        data: [products[0].numberTimesClicked, products[1].numberTimesClicked, products[2].numberTimesClicked, products[3].numberTimesClicked, products[4].numberTimesClicked, products[5].numberTimesClicked, products[6].numberTimesClicked, products[7].numberTimesClicked, products[8].numberTimesClicked, products[9].numberTimesClicked, products[10].numberTimesClicked, products[11].numberTimesClicked, products[12].numberTimesClicked, products[13].numberTimesClicked, products[14].numberTimesClicked, products[15].numberTimesClicked, products[16].numberTimesClicked, products[17].numberTimesClicked, products[18].numberTimesClicked, products[19].numberTimesClicked],
+        data: productVotes,
         backgroundColor: [
           '#183A2B',
           '#1E4835',
@@ -280,6 +273,8 @@ function displayResults() {
       scales: {
         yAxes: [{
           ticks: {
+            fixedStepSize: 1,
+            stepSize: 1,
             beginAtZero:true
           }
         }]
